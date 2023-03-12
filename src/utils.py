62213ef6fc2ae168fd958746649f71e9a61a9317
Alin <alin.alex.b@gmail.com>
2023-03-12 16:20:18 +0200

    added delete on startup option for data

--- src/utils.py
+++ src/utils.py
@@ -7,6 +7,7 @@ logger = logging.getLogger(__name__)
 
 
 MAX_RECORDS = os.environ.get('MAX_RECORDS')
+DELETE_ON_STARTUP = os.environ.get('DELETE_ON_STARTUP')
 if isinstance(MAX_RECORDS, str):
     MAX_RECORDS = int(MAX_RECORDS)
 FILE = 'data'
@@ -17,7 +18,7 @@ class Records:
     max_records = MAX_RECORDS
 
     def __init__(self):
-        self.init_from_file()
+        self.init_from_file(DELETE_ON_STARTUP)
 
     def update_limit_if_changed(self, old_limit):
         if old_limit != self.max_records:
@@ -28,8 +29,8 @@ class Records:
         if len(self.records) >= self.max_records:
             self.records = []
 
-    def init_from_file(self):
-        if not path.isfile(FILE):
+    def init_from_file(self, delete_on_startup=False):
+        if delete_on_startup or not path.isfile(FILE):
             self.records = []
             return
         with open(FILE, 'rb') as f:
