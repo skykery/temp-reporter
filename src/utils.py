6308c873a03fefc6e0b14e08e903a5fe38a117c5
Alin <alin.alex.b@gmail.com>
2023-03-12 16:06:34 +0200

    updated limit if changed

--- src/utils.py
+++ src/utils.py
@@ -17,6 +17,11 @@ class Records:
     def __init__(self):
         self.init_from_file()
 
+    def update_limit_if_changed(self, old_limit):
+        if old_limit != self.max_records:
+            return self.max_records
+        return old_limit
+
     def reset_if_is_full(self):
         if len(self.records) >= self.max_records:
             self.records = []
@@ -27,7 +32,7 @@ class Records:
             return
         with open(FILE, 'rb') as f:
             item = pickle.load(f)
-            self.records, self.max_records = item['records'], item['max_records']
+            self.records, self.max_records = item['records'], self.update_limit_if_changed(item['max_records'])
 
     def write(self, item: dict):
         self.reset_if_is_full()
